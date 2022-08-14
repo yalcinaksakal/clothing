d41693ca803a83e421e67622c8e347c03f97d614
yalcinaksakal <yalcin.aksakal.tr@gmail.com>
2022-08-14 18:58:08 +0200

    emptu cart

--- src/routes/checkout/checkout.jsx
+++ src/routes/checkout/checkout.jsx
@@ -1,11 +1,17 @@
 import "./checkout.scss";
 import { useSelector } from "react-redux";
 import CheckoutItem from "../../components/checkout-item/checkout-item";
+import Button from "../../components/button/button";
+import { useNavigate } from "react-router-dom";
 
 const headers = ["Product", "Description", "Quantity", "Price", "Remove"];
 const Checkout = () => {
-	const { cart, total } = useSelector(state => state.cart);
-	return (
+	const { cart, total } = useSelector(state => state.cart),
+		navigate = useNavigate(),
+		navigator = () => {
+			navigate("/");
+		};
+	return total ? (
 		<div className="checkout-container">
 			<div className="checkout-header">
 				{headers.map((h, i) => (
@@ -20,6 +26,13 @@ const Checkout = () => {
 					<CheckoutItem key={item.id} item={item} />
 				))}
 			<span className="total">Total: {total} €</span>
+		</div>
+	) : (
+		<div className="checkout-container">
+			<p>Your cart is empty.</p>
+			<Button buttonType="inverted" onClick={navigator}>
+				Home
+			</Button>
 		</div>
 	);
 };
